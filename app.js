f0c46f5d84d662fa6bac4057bd3409ce8e608988
Tristan Ratchford <tristan.ratchford@gmail.com>
2015-04-04 13:27:36 -0400

    fixed port issue

--- app.js
+++ app.js
@@ -13,11 +13,11 @@ app.get('/', function (req, res) {
   });
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
 
   var host = server.address().address;
-  var port = process.env.PORT || 3000;
+  var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+
 
 });

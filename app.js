2f99b30e0075a9786b50d160d9aaaaf67b6a9b8c
Tristan Ratchford <tristan.ratchford@gmail.com>
2015-04-04 13:22:34 -0400

    fixed port

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.get('/', function (req, res) {
 var server = app.listen(3000, function () {
 
   var host = server.address().address;
-  var port = server.address().port;
+  var port = process.env.PORT || 3000;
 
   console.log('Example app listening at http://%s:%s', host, port);
 
